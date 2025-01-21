4c67b0ddd1efd57dfaaad01ee8d06bd8e6a14a7d
militaovitor01 <vdiasmilitao@gmail.com>
2025-01-20 23:25:39 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const html = document.querySelector('html')
+const botaoFoco = document.querySelector('.app__card-button--foco')
+const botaoDescansoCurto = document.querySelector('.app__card-button--curto')
+const botaoDescansoLongo = document.querySelector('.app__card-button--longo')
+const imagem = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const listaBotoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () =>{
+    if(musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
+
+botaoFoco.addEventListener('click', () =>{
+    alterarContexto('foco')
+    botaoFoco.classList.add('active')
+})
+
+botaoDescansoCurto.addEventListener('click', () =>{
+    alterarContexto('descanso-curto')
+    botaoDescansoCurto.classList.add('active')
+})
+
+botaoDescansoLongo.addEventListener('click', () =>{
+    alterarContexto('descanso-longo')
+    botaoDescansoLongo.classList.add('active')
+})
+
+function alterarContexto(contexto) {
+    listaBotoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+
+    html.setAttribute('data-contexto', contexto)
+    imagem.setAttribute('src', `/imagens/${contexto}.png`)
+
+    switch(contexto){
+        case "foco":
+            titulo.innerHTML = `
+                Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>                
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+                Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+                Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa!</strong>
+            `
+        default:
+            break;
+    }        
+}
